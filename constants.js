85f4abf23b56e8d1c23bdbc249531f4ce244a100
junaidalam2 <junaid.alam2@gmail.com>
2023-10-21 16:35:28 -0700

    pseudo code to main.js and created constants.js

---
+++ constants.js
@@ -0,0 +1,23 @@
+
+
+
+//Truck Info
+costPerMile = 1.816923077
+
+//Standard Package
+stdPackageWeight = 66 // lbs
+stdPackageVolume = 18 // cubic feet
+
+//Pallet Info
+palletWeight = 440 // lbs
+palletVolume = 64 // cubic feet
+
+
+
+//Cargo Info.
+maxWeight = 9180 // lbs
+truckVolume = 1700 // cubic feet
+truckPalletsMax = truckVolume / palletVolume
+palletCostPerMile = truckPalletsMax / costPerMile
+stdPackagePerTruck = truckVolume / stdPackageVolume
+stdPackagePerMile = stdPackagePerTruck / costPerMile
--- main.js
+++ main.js
@@ -1,3 +1,39 @@
 
 
 
+/*
+
+-ket inputs to new order:
+    -startpoint, end point, volume, weight
+    
+    things to add later:
+    -should add date later when make it multi-day 
+    -type
+
+-check constraints:
+
+1. On route
+2. Volume (by pallet)
+3. Weight
+4. time
+
+ things to add later:
+ -compatible type of package
+
+
+-Output:
+    -if will accept order
+    -price
+    -date
+
+
+-features to add later:
+    -mutli-day
+    -other routes
+
+
+*/
+
+
+// Route 1
+
