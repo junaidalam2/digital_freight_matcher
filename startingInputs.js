ec5c08e197decf35aa0b170714d05266daa5e57c
junaidalam2 <junaid.alam2@gmail.com>
2023-10-28 08:05:57 -0700

    added capacity data

--- startingInputs.js
+++ startingInputs.js
@@ -8,19 +8,23 @@ class Route {
 
         this.routeNumber = routeNumber;
         this.anchorPoint = anchorPoint;
-        this.longitude = longitude; // of anchor
-        this.latitude = latitude; // of anchor
+        this.anchorLongitude = longitude;
+        this.anschorLatitude = latitude;
         this.milesWithCargo = milesWithCargo;
         this.totalMiles = this.milesWithCargo * 2;
         this.operationalTruckCost = constants.costPerMile * this.totalMiles;
         this.palletsOccupied = palletsOccupied;
         this.cargoCost = this.totalMiles * constants.palletCostPerMile * this.palletsOccupied;
         this.emptyCargoCost = this.operationalTruckCost - this.cargoCost;
-        this.price = this.operationalTruckCost * constants.markup;
+        this.price = this.cargoCost * constants.markup; // based on cargo cost
         this.pickUpDropOffCounter =  pickUpDropOffCounter;
+        this.availableVolume = this.palletsOccupied * constants.palletVolume * constants.truckVolume;
+        this.availableWeight = constants.maxWeight - this.palletsOccupied * constants.palletWeight;
+        this.availableStandardPackages = this.availableVolume / constants.stdPackageVolume
+        this.projectPricePerPackage = this.emptyCargoCost / this.availableStandardPackages * ( 1 + constants.markup);
+        this.projectedRevenueFullTruck =  projectPricePerPackage * this.availableStandardPackages + this.price 
 
     }
-
 }
 
 const route1 = new Route(1, 'Ringgold', 101, 12, 2)
